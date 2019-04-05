00c0a89167aa97291d4008b660aa957eef8528ee
Simon Konglevoll Lønnestad <simon.k.lonnestad@gmail.com>
2019-04-05 02:06:33 +0200

    Update CREATE_LOCATION_SP.sql

--- CREATE_LOCATION_SP.sql
+++ CREATE_LOCATION_SP.sql
@@ -8,48 +8,58 @@ create or replace procedure CREATE_LOCATION_SP (
     p_location_administrative_region    IN  VARCHAR
 )
 IS
-    ex_exception    EXCEPTION;
-    ex_error_msg    VARCHAR(100);
-    location_count  INTEGER;
+    ex_exception        EXCEPTION;
+    ex_error_msg        VARCHAR(100);
+    checked_location_id NUMBER;
+
+    CURSOR check_location_duplicate IS
+        SELECT LOCATION_ID FROM VM_LOCATION WHERE location_country = p_location_country AND location_postal_code = p_location_postal_code AND location_street_1 = p_location_street1;
 
     BEGIN
         IF p_location_country IS NULL THEN
             ex_error_msg := 'Missing country! Can not be NULL';
             RAISE ex_exception;
-        END IF;
-
-        IF p_location_postal_code IS NULL THEN
+        ELSIF p_location_postal_code IS NULL THEN
             ex_error_msg := 'Missing postal code! Can not be NULL';
             RAISE ex_exception;
-        END IF;
-
-        SELECT COUNT(*) INTO location_count
-        FROM VM_LOCATION
-        WHERE location_country = p_location_country AND location_postal_code = p_location_postal_code AND location_street_1 = p_location_street1;
-
-        IF location_count >= 1 THEN
-            SELECT LOCATION_ID
-            INTO p_location_id
-            FROM VM_LOCATION
-            WHERE location_country = p_location_country AND location_postal_code = p_location_postal_code AND location_street_1 = p_location_street1;
-
-            ex_error_msg := 'This location already exist. Location ID: ' || p_location_id;
-            RAISE ex_exception;
         ELSE
-            p_location_id := LOCATION_ID_SEQ.NEXTVAL;
+            OPEN check_location_duplicate;
+            FETCH check_location_duplicate INTO checked_location_id;
+                IF check_location_duplicate%FOUND THEN
+                    p_location_id := checked_location_id;
+                    RAISE DUP_VAL_ON_INDEX;
+                ELSE
+                    p_location_id := LOCATION_ID_SEQ.NEXTVAL;
 
-            INSERT INTO VM_LOCATION(LOCATION_ID, LOCATION_COUNTRY, LOCATION_POSTAL_CODE, LOCATION_STREET_1, LOCATION_STREET_2, LOCATION_CITY, LOCATION_ADMINISTRATIVE_REGION)
-            VALUES (p_location_id, p_location_country, p_location_postal_code, p_location_street1, p_location_street2, p_location_city, p_location_administrative_region);
-            COMMIT;
+                    INSERT INTO VM_LOCATION
+                    VALUES (
+                        p_location_id,
+                        p_location_country,
+                        p_location_postal_code,
+                        p_location_street1,
+                        p_location_street2,
+                        p_location_city,
+                        p_location_administrative_region,
+                        NULL,
+                        NULL
+                    );
+                    COMMIT;
 
-            DBMS_OUTPUT.PUT_LINE('Location added with ID: ' || p_location_id);
+                    DBMS_OUTPUT.PUT_LINE('Location added with ID: ' || p_location_id);
+                END IF;
+            CLOSE check_location_duplicate;
         END IF;
 
         EXCEPTION
+            WHEN DUP_VAL_ON_INDEX THEN
+                DBMS_OUTPUT.PUT_LINE('This location already exists! Location ID: ' || p_location_id);
+                ROLLBACK;
             WHEN ex_exception THEN
-                DBMS_OUTPUT.PUT_LINE (ex_error_msg);
+                DBMS_OUTPUT.PUT_LINE(ex_error_msg);
                 ROLLBACK;
             WHEN OTHERS THEN
                 DBMS_OUTPUT.PUT_LINE('An error occured inserting new location!');
+                dbms_output.put_line('Error code: ' || sqlcode);
+                dbms_output.put_line('Error message: ' || sqlerrm);
                 ROLLBACK;
     END;
